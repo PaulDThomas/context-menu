f632527b7bb3f1503b86ddc76422b27fbc98d747
Paul Thomas <paul@asup.co.uk>
2025-05-28 12:30:58 +0100

    Add and remove transition div

--- src/components/AutoHeight.tsx
+++ src/components/AutoHeight.tsx
@@ -16,29 +16,41 @@ export function AutoHeight({
   const wrapperRef = useRef<HTMLDivElement>(null);
   const innerRef = useRef<HTMLDivElement>(null);
   const transitionRef = useRef<HTMLDivElement>(null);
-  const [deployedChildren, setDeployedChildren] = useState<React.ReactNode>(children);
+  const [deployedChildren, setDeployedChildren] = useState<React.ReactNode>(null);
   const [height, setHeight] = useState<number | null>(null);
-  const isResizing = useRef<"resize" | null>(null);
+  const [isResizing, setIsResizing] = useState<"draw" | "resize" | "remove" | null>(null);
 
   // Trigger height change on children update
   useLayoutEffect(() => {
     const wrapper = wrapperRef.current;
     const inner = innerRef.current;
-    const transition = transitionRef.current;
-    if (!wrapper || !inner || !transition) return;
+
+    // Nothing is drawn yet
+    if (!wrapper || !inner) return;
+
+    // No transitionRef means we are in the initial draw phase
+    if (!transitionRef.current && deployedChildren !== children) {
+      setIsResizing("draw");
+      return;
+    }
+    // End the transition, this will remove the transitionRef from the DOM
+    if (isResizing === "remove") {
+      setIsResizing(null);
+      return;
+    }
+
+    const transition = transitionRef?.current;
+    if (!transition) return;
 
     // Initial draw to get the height of children and deployed children
     const childrenHeight = inner.offsetHeight ?? 0;
     const deployedHeight = transition.offsetHeight ?? 0;
 
-    inner.dataset.height = `${childrenHeight}`;
-    transition.dataset.height = `${deployedHeight}`;
-
     // if deployed children height is different, update the state
     if (childrenHeight !== deployedHeight) {
-      switch (isResizing.current) {
-        case null: {
-          isResizing.current = "resize";
+      switch (isResizing) {
+        case "draw": {
+          setIsResizing("resize");
           break;
         }
         case "resize":
@@ -49,17 +61,23 @@ export function AutoHeight({
       const handleTransitionEnd = () => {
         wrapper.removeEventListener("transitionend", handleTransitionEnd);
         setDeployedChildren(children);
-        isResizing.current = null;
+        setIsResizing("remove");
       };
 
       wrapper.addEventListener("transitionend", handleTransitionEnd);
-    } else if (childrenHeight !== height) {
+    }
+    // if children height is different, update the state
+    else if (childrenHeight !== height) {
       setHeight(childrenHeight);
       setDeployedChildren(hide || !children ? <div style={{ height: "1px" }} /> : children);
-    } else if (children !== deployedChildren) {
-      setDeployedChildren(children);
+      setIsResizing("remove");
     }
-  }, [children, deployedChildren, duration, height, hide]);
+    // if just the children have changed, update the deployed children
+    else if (children !== deployedChildren) {
+      setDeployedChildren(children);
+      setIsResizing("remove");
+    }
+  }, [children, deployedChildren, height, hide, isResizing]);
 
   return (
     <div
@@ -74,24 +92,20 @@ export function AutoHeight({
     >
       <div
         ref={innerRef}
-        className={[
-          styles.autoHeightInner,
-          isResizing.current ? styles[isResizing.current] : "",
-        ].join(" ")}
-        style={isResizing.current ? { transition: `opacity ${duration}ms ease-in-out` } : undefined}
+        className={[styles.autoHeightInner, isResizing ? styles[isResizing] : ""].join(" ")}
+        style={isResizing ? { transition: `opacity ${duration}ms ease-in-out` } : undefined}
       >
         {deployedChildren}
       </div>
-      <div
-        className={[
-          styles.autoHeightTransition,
-          isResizing.current ? styles[isResizing.current] : "",
-        ].join(" ")}
-        ref={transitionRef}
-        style={isResizing.current ? { transition: `opacity ${duration}ms ease-in-out` } : undefined}
-      >
-        {hide || !children ? <div style={{ height: "1px" }} /> : children}
-      </div>
+      {isResizing && (
+        <div
+          className={[styles.autoHeightTransition, isResizing ? styles[isResizing] : ""].join(" ")}
+          ref={transitionRef}
+          style={isResizing ? { transition: `opacity ${duration}ms ease-in-out` } : undefined}
+        >
+          {hide || !children ? <div style={{ height: "1px" }} /> : children}
+        </div>
+      )}
     </div>
   );
 }
