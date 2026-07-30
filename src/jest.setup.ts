d25f9e101077dfc05c9be3f6481e7da7fb41169b
Paul Thomas <paul@asup.co.uk>
2026-07-30 07:40:31 +0100

    npm update

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,3 +1,3 @@
 // Configure React 19's act() environment for testing
 // This must be set before any React imports
-(global as typeof globalThis).IS_REACT_ACT_ENVIRONMENT = true;
+(globalThis as { IS_REACT_ACT_ENVIRONMENT?: boolean }).IS_REACT_ACT_ENVIRONMENT = true;
